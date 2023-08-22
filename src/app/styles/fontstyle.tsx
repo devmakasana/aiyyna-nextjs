b6e1342f2b041b5aeabbf7d23717d3e162508073
trupal-pixer <119833171+trupal-pixer@users.noreply.github.com>
2023-08-22 17:04:57 +0530

    create font size component

---
+++ src/app/styles/fontstyle.tsx
@@ -0,0 +1,64 @@
+import { css } from 'styled-components';
+
+const Heading1 = css`
+  font-family: 'Euclid Circular B SemiBold';
+  font-size: 58px;
+  font-weight: 600;
+  line-height: 74px;
+`;
+const Heading2 = css`
+  font-family: 'Euclid Circular B SemiBold';
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 46px;
+`;
+const Heading3 = css`
+  font-family: 'Euclid Circular B Medium';
+  font-size: 28px;
+  font-weight: 500;
+  line-height: 36px;
+`;
+const Heading4 = css`
+  font-family: 'Euclid Circular B Medium';
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 26px;
+`;
+const Heading5 = css`
+  font-family: 'Euclid Circular B SemiBold';
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 23px;
+`;
+const Body1 = css`
+  font-family: 'Inter';
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 170%;
+`;
+const Body2 = css`
+  font-family: 'Inter';
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 160%;
+`;
+const ButtonText = css`
+  font-family: 'Inter';
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 20px;
+`;
+const InputTitle = css`
+  font-family: 'Inter';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 17px;
+`;
+const SmallText = css`
+  font-family: 'Inter';
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 17px;
+  letter-spacing: 1px;
+`;
+export { Heading1, Heading2, Heading3, Heading4, Heading5, Body1, Body2, ButtonText, InputTitle, SmallText };
