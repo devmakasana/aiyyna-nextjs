7bcb59d3b79c5492c0d1beacaa3a5b9c319590c8
trupal-pixer <119833171+trupal-pixer@users.noreply.github.com>
2023-08-22 13:02:02 +0530

    change file format

---
+++ src/app/components/pagecomponent/homepage/index.tsx
@@ -0,0 +1,25 @@
+import Header from '@/app/components/header/header';
+
+import Herosection from '@/app/components/herosection';
+import Features from '@/app/components/Features';
+import Integrations from '@/app/components/integrations';
+import FooterCTA from '@/app/components/footerCta/footercta';
+import Footer from '@/app/components/footer/footer';
+import { Group, Hero } from '@/app/styles/homepage';
+import Testimonialsection from '@/app/components/testimonialSection/testimonialsection';
+
+export default function Homepage() {
+  return (
+    <Group>
+      <Hero>
+        <Header />
+        <Herosection />
+      </Hero>
+      <Features />
+      <Integrations />
+      <Testimonialsection />
+      <FooterCTA />
+      <Footer />
+    </Group>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Homepage from '@/components/pagecomponent/homepage';
+import Homepage from '@/app/components/pagecomponent/homepage';
 import styles from './page.module.css';
 
 export default function Home() {
