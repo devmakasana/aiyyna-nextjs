4f786f021cf156d132a10f0d8a790349d51a469a
trupal-pixer <119833171+trupal-pixer@users.noreply.github.com>
2023-08-29 17:35:51 +0530

    blog detail page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import Layout from "@/app/components/layout";
+import BlogdetailPage from "@/app/components/pagecomponent/blogdetailpage";
 
 export default function BlogDetail() {
     return (
         <Layout>
-            <div>blog detail page</div>
+            {/* <div>blog detail page</div> */}
+            <BlogdetailPage />
         </Layout>
     );
 }
