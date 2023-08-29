12ef8d847ef02160f9ef323c5b424a9237da5478
Krishna-pixer <krishna@pixer.digital>
2023-08-29 17:26:11 +0530

    create a contactus page

--- src/app/components/pagecomponent/contactpage/index.tsx
+++ src/app/components/pagecomponent/contactpage/index.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Group, Hero } from '@/app/styles/contactpage';
-import FooterCTA from '@/app/components/footerCta/footercta';
-import Footer from '@/app/components/footer/footer';
 import Header from '../../header/header';
 import Contactsection from '../../contactSection/contactsection';
 export default function Contactpage() {
   return (
-    <Group>
-      <Hero>
+    <div className='group'>
+      <div className='contacthero'>
         <Header />
         <Contactsection />
-      </Hero>
-      {/* <FooterCTA description={'Let’s simplify & automate your marketing campaigns'} title={'Book A Demo'} /> */}
-      <Footer />
-    </Group>
+      </div>
+    </div>
   );
 }
--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,9 +1,10 @@
-import Layout from "@/app/components/layout";
+import Layout from '@/app/components/layout';
+import Contactpage from '../components/pagecomponent/contactpage';
 
 export default function ContactUS() {
-    return (
-        <Layout>
-            <div>Contact us page</div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Contactpage />
+    </Layout>
+  );
 }
