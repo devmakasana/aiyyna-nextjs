4a481a55f5d77d84900c877eaba7de9303e5fded
Krishna-pixer <krishna@pixer.digital>
2023-08-29 16:42:40 +0530

    create termscondition hero section

--- src/app/components/pagecomponent/termsandconditionspage/index.tsx
+++ src/app/components/pagecomponent/termsandconditionspage/index.tsx
@@ -1,28 +1,19 @@
-'use client';
-
 import React from 'react';
 import Header from '../../header/header';
 import { Group, Heading, Hero, Title } from '@/app/styles/termsandConditions';
 import FooterCTA from '../../footerCta/footercta';
 import Termsandconditionsdata from '../../termsandconditionsdata';
 import Footer from '../../footer/footer';
+import Termsconditionhero from '../../termsconditionshero/termsconditionhero';
 
 export default function TermsandConditionsPage() {
   return (
     <div className='group'>
       <div className='termsconditionshero'>
         <Header />
-        <Heading>
-          {' '}
-          <Title>Terms & Conditions </Title>
-        </Heading>
+        <Termsconditionhero />
       </div>
       <Termsandconditionsdata />
-      {/* <FooterCTA
-        description={'Drop us a line or two, we are open for creative minds and collaborations!'}
-        title={'Contact Us'}
-      /> */}
-      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/app/components/termsconditionshero/termsconditionhero.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import { Heading, Title } from './styles';
+
+export default function Termsconditionhero() {
+  return (
+    <div>
+      <Heading>
+        <Title>Terms & Conditions </Title>
+      </Heading>
+    </div>
+  );
+}
