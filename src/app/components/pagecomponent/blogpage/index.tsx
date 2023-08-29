35c88f9a9e3c9421b56e09a7b9ff67f0accdbdc1
Krishna-pixer <krishna@pixer.digital>
2023-08-29 17:42:22 +0530

    create a blog hero section

--- src/app/components/pagecomponent/blogpage/index.tsx
+++ src/app/components/pagecomponent/blogpage/index.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React from 'react';
 import { Group, Hero, Title } from '@/app/styles/blogpage';
 import FooterCTA from '@/app/components/footerCta/footercta';
@@ -6,12 +5,14 @@ import Footer from '@/app/components/footer/footer';
 import Header from '../../header/header';
 import Mainblogcard from '../../mainBlogCard/mainblogcard';
 import Blogallcard from '../../blogAll/blogallcard';
+import Bloghero from '../../blogHero/bloghero';
 export default function Blogpage() {
   return (
     <div className='group'>
       <div className='hero'>
         <Header />
-        <Title>Blog</Title>
+        {/* <Title>Blog</Title> */}
+        <Bloghero />
       </div>
       <Mainblogcard
         imgSrc={'/images/mainblog.png'}
