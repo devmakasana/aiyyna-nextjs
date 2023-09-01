04dbecd8c3a70481eb382a6121e2e124a646fff4
nidhi-pixer <119833181+nidhi-pixer@users.noreply.github.com>
2023-09-01 16:07:32 +0530

    responsive design for blog detail page screen

--- src/app/components/blogdetailHero/blogdetailhero.tsx
+++ src/app/components/blogdetailHero/blogdetailhero.tsx
@@ -2,17 +2,18 @@
 import React from 'react';
 import Image from 'next/image';
 import { Description, Heading, Mainimage, Title } from './styles';
+import { Container } from '@/app/styles/commoncontainer';
 
 export default function Blogdetailhero() {
   return (
-    <div>
+    <Container>
       <Heading>
         <Title>09 Feb, 2022</Title>
         <Description>A Beginner's Guide To Creating A Solid Foundation For Your Sales Operations</Description>
       </Heading>
       <Mainimage>
-        <Image src={'/images/blogdetail.svg'} alt={'blogdetail'} width={980} height={653} />
+        <Image src={'/images/blogdetail.svg'} alt={'blogdetail'} width={980} height={653} className='blogimage' />
       </Mainimage>
-    </div>
+    </Container>
   );
 }
