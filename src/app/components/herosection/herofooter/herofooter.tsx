d9253176ffb4af2ccc03259d62662106b28d788c
harshal-pixer <harshal.chaklasiya@pixer.digital>
2023-09-04 17:08:03 +0530

    set map method in useMemo hook and get all features data in home page

--- src/app/components/herosection/herofooter/herofooter.tsx
+++ src/app/components/herosection/herofooter/herofooter.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Content, Icons, Maindiv, Title } from './styles';
 import Image from 'next/image';
 import { Container } from '@/app/styles/commoncontainer';
@@ -13,18 +13,18 @@ export default function Herofooter({
   companyLogos?: CompanySectionContentCollectionItemsModel[];
   companyTitle?: string;
 }) {
+  const renderCompanyLogos = useMemo(() => {
+    return companyLogos?.map((item, index: number) => {
+      return <Image key={index} src={item?.logo?.url} alt='logoipsum' width={159} height={42} className='svg-forth' />;
+    });
+  }, []);
+
   return (
     <Container>
       <Content>
         <Maindiv>
           {!isEmpty(companyTitle) && <Title>{companyTitle}</Title>}
-          {!isEmpty(companyLogos) && (
-            <Icons>
-              {companyLogos?.map((item) => {
-                return <Image src={item?.logo?.url} alt='logoipsum' width={159} height={42} className='svg-forth' />;
-              })}
-            </Icons>
-          )}
+          {!isEmpty(companyLogos) && <Icons>{renderCompanyLogos}</Icons>}
         </Maindiv>
       </Content>
     </Container>
--- src/app/components/pagecomponent/home/homePage.tsx
+++ src/app/components/pagecomponent/home/homePage.tsx
@@ -4,20 +4,17 @@ import Integrations from '@/app/components/integrations/integrations';
 import Testimonialsection from '@/app/components/testimonialSection/testimonialsection';
 import Header from '../../header/header';
 import { isEmpty } from '@/app/helper/helper';
-import { HomePageDetail } from '@/app/model/homePageModel';
+import { FeatureSectionContentCollectionItemsModel, HomePageDetail } from '@/app/model/homePageModel';
 
-export default function Homepage({ details }: { details: HomePageDetail }) {
+export default function Homepage({ details, featuresData }: { details: HomePageDetail; featuresData: FeatureSectionContentCollectionItemsModel[] }) {
   return (
     <div className='group'>
       <div className='homehero'>
         <Header />
         <Herosection details={details} />
       </div>
-      {!isEmpty(details?.featureSectionContentCollection?.items) && !isEmpty(details?.featureSectionTitle) && (
-        <Features
-          featuresData={details?.featureSectionContentCollection?.items}
-          featuresTitle={details?.featureSectionTitle}
-        />
+      {!isEmpty(featuresData) && !isEmpty(details?.featureSectionTitle) && (
+        <Features featuresData={featuresData} featuresTitle={details?.featureSectionTitle} />
       )}
       {!isEmpty(details?.intigrationTitle) && <Integrations IntegrationTitle={details?.intigrationTitle} />}
       {!isEmpty(details?.testinomialSectionContentCollection?.items) && !isEmpty(details?.testinomialSectionTitle) && (
