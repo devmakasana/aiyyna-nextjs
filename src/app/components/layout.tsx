e4430786ced444fb0c5e5b8fb9e385f0242615eb
harshal-pixer <harshal.chaklasiya@pixer.digital>
2023-09-04 12:21:40 +0530

    fix UI rendering problem when we refresh the page

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,19 +1,28 @@
+'use client';
+import React, { useEffect, useState } from 'react';
 import Footer from './footer/footer';
 import FooterCTA from './footerCta/footercta';
-import Header from './header/header';
 
-export default async function Layout({
+export default function Layout({
   children = <></>,
   CTAtitle = 'Drop us a line or two, we are open for creative minds and collaborations!',
   CTAbtntext = 'Contact Us'
 }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <>
-      <div>
-        <main>{children}</main>
-        <FooterCTA CTAtitle={CTAtitle} CTAbtntext={CTAbtntext} />
-        <Footer />
-      </div>
+      {isClient && (
+        <div>
+          <main>{children}</main>
+          <FooterCTA CTAtitle={CTAtitle} CTAbtntext={CTAbtntext} />
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
