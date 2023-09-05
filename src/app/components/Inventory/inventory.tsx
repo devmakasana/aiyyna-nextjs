6d81b92d57bd5b2905da4d92c3ec1918b249866b
harshal-pixer <harshal.chaklasiya@pixer.digital>
2023-09-05 12:41:17 +0530

    remove unwanted comment

--- src/app/components/Inventory/inventory.tsx
+++ src/app/components/Inventory/inventory.tsx
@@ -14,14 +14,6 @@ export default function Inventory({ itemDetails }: { itemDetails: ServiceSection
           {!isEmpty(itemDetails?.title) && <Title>{itemDetails?.title}</Title>}
           <Points>
             {!isEmpty(itemDetails?.description) && <Description>{itemDetails?.description}</Description>}
-            {/* <Description>
-              <span>2.</span> If you forgot to cancel Aiyyna a couple months ago, and you haven’t used Aiyyna since
-              then, we’ll give you a full refund for a few back months. No problem.
-            </Description>
-            <Description>
-              <span>3.</span> If you tried Aiyyna for a couple months, and you just weren’t happy with it, you can have
-              your money back.
-            </Description> */}
           </Points>
         </Leftdiv>
         {!isEmpty(itemDetails?.banner?.url) && (
